908351d011555f5bd7121374c4d4dc612b7fe75c
henriquedevop <wymuxgh@gmail.com>
2024-12-04 14:50:15 -0300

    feat: fetch product data from local API and render on screen

--- src/components/products/index.tsx
+++ src/components/products/index.tsx
@@ -1,6 +1,28 @@
 import { BiCart } from "react-icons/bi"
+import { useEffect, useState } from "react"
+import { api } from "../../services/api"
+
+interface ProductsProps {
+    id: number;
+    title: string;
+    description: string;
+    price: number;
+    cover: string;
+}
 
 export function Products() {
+
+    const [products, setProducts] = useState<ProductsProps[]>([])
+
+    useEffect(() => {
+        async function getData() {
+            const response = await api.get("/products")
+            setProducts(response.data)
+        }
+
+        getData()
+    },[])
+
     return (
         <div>
 
@@ -9,20 +31,25 @@ export function Products() {
                 <h1 className="font-bold text-2xl mb-4 mt-10 text-center">Produtos em alta</h1>
 
                 <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-5">
-                    <section className="w-full">
-                        <img
-                        className="w-full rounded-lg max-h-70 bg-2"
-                        alt="imagem do produto"
-                        src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/airpods-pro-2-hero-select-202409_FV1_FMT_WHH?wid=752&hei=636&fmt=jpeg&qlt=90&.v=1725492498882"
-                        />
-                        <p className="font-medium mt-1 mb-2">Air pods pro</p>
-                        <div className="flex gap-3 items-center">
-                            <strong className="text-zinc-700/90">R$ 1000</strong>
-                            <button className="bg-zinc-900 p-1 rounded">
-                                <BiCart size={20} color="#fff"/>
-                            </button>
-                        </div>
-                    </section>
+                    {products.map((item) => (
+                        <section key={item.id} className="w-full">
+                            <img
+                            className="w-full rounded-lg max-h-70 bg-2"
+                            alt={item.title}
+                            src={item.cover}
+                            />
+                            <p className="font-medium mt-1 mb-2">{item.title}</p>
+                            <div className="flex gap-3 items-center">
+                                <strong className="text-zinc-700/90">{item.price.toLocaleString("pt-BR", {
+                                    style: "currency",
+                                    currency: "BRL"
+                                })}</strong>
+                                <button className="bg-zinc-900 p-1 rounded">
+                                    <BiCart size={20} color="#fff"/>
+                                </button>
+                            </div>
+                        </section>
+                    ))}
                 </div>
 
             </main>
